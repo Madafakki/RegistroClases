f95b033549dd00169e06ec343a7d086c9dd94fdf
Madafakki <josa.morales@duocuc.cl>
2023-09-13 15:55:43 -0300

    Listisimo profe

--- src/app/clase/clase.page.ts
+++ src/app/clase/clase.page.ts
@@ -11,6 +11,12 @@ export class ClasePage implements OnInit {
   usuario: string = '';
   contrasena: string = '';
   qrData: string = '';
+  profesor: string = '';
+  hora: string = '';
+  sala: string = '';
+  dia: string = '';
+
+  // Para acceder a datos específicos, puedes hacer algo como esto:
 
   constructor(private router: Router) { }
 
@@ -19,9 +25,20 @@ export class ClasePage implements OnInit {
     if (parametros?.extras.state) {
       this.usuario = parametros?.extras.state['user'];
       this.contrasena = parametros?.extras.state['pass'];
-      this.qrData = parametros?.extras.state['qrData']
+      this.qrData = parametros?.extras.state['qrData'];
+
+      // Dividir la qrData cuando tengas un valor
+      if (this.qrData) {
+        const partes = this.qrData.split(',');
+
+        // Asegúrate de que haya suficientes partes antes de acceder a ellas
+        if (partes.length >= 4) {
+          this.profesor = partes[0].split(':')[1].trim();
+          this.hora = partes[1].split(':')[1].trim();
+          this.sala = partes[2].split(':')[1].trim();
+          this.dia = partes[3].split(':')[1].trim();
+        }
+      }
     }
   }
-
-
 }
